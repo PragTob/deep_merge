aa56910ba594a38ee0a7a3c71e66d1856280f065
Tobias Pfeiffer <pragtob@gmail.com>
2016-10-20 22:43:20 +0200

    support elixir 1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DeepMerge.Mixfile do
   def project do
     [app: :deep_merge,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
