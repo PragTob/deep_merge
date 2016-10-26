8d82ea7ecd283a1d9e220aa87a46d67807cfeb42
Tobias Pfeiffer <pragtob@gmail.com>
2016-10-26 18:08:30 +0200

    Document the DeepMerge module

--- lib/deep_merge.ex
+++ lib/deep_merge.ex
@@ -1,5 +1,23 @@
 defmodule DeepMerge do
+  @moduledoc """
+  Provides functionality for deeply/recursively merging entites (normally for
+  `Map` and `Keyword`).
+
+  If you want to change the behaviour of a custom struct or something similar,
+  please have a look at the `DeepMerge.Resolver` protocol.
+  """
+
   @doc """
+  Deeply merges two maps or keyword list, meaning that if two conflicting values
+  are maps or keyword lists themselves then they will also be merged.
+
+  This is rather similar to `Map.merge/2` and `Keyword.merge/2`. However, it
+  always applies merging to both maps and keyword lists and also merges those
+  recursively.
+
+  It does not merge structs or structs with maps. If you want structs to be
+  merged then please have a look at the `DeepMerge.Resolver` protocol and
+  consider implementing it.
 
   ## Examples
 
