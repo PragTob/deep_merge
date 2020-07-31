a4e74eca5e2833863d26c958479fe3f714590e9a
Tobias Pfeiffer <pragtob@gmail.com>
2020-07-31 08:24:29 +0200

    upgrade all the deps

--- mix.exs
+++ mix.exs
@@ -53,7 +53,7 @@ defmodule DeepMerge.Mixfile do
       {:ex_doc, "~> 0.11", only: :dev},
       {:earmark, "~> 1.2", only: :dev},
       {:excoveralls, "~> 0.7", only: :test},
-      {:inch_ex, "~> 0.5", only: :docs},
+      {:inch_ex, "~> 2.0", only: :docs},
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false}
     ]
   end
