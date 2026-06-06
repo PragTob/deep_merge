82c1a7b0739deae82a74831ff4b28a21e18ccf6a
Tobias Pfeiffer <pragtob@gmail.com>
2026-06-06 10:05:16 +0200

    Fix getting the implementors correctly
    Not sure why I didn't do this before - maybe `impl_for` is younger
    than the library?
    
    Discovered that tests were failing for `any` with coveralls/
    coverage but not the normal testing probably due to how it
    interacted with Protocols/loading and then broke consolidation
    and so this as well.

--- lib/deep_merge/resolver.ex
+++ lib/deep_merge/resolver.ex
@@ -94,29 +94,16 @@ defimpl DeepMerge.Resolver, for: Any do
   @doc """
   Fall back to always taking the override.
 
-  Also the implementation for `@derive [DeppMerge.Resolver]` where structs of the same type that
+  Also the implementation for `@derive [DeepMerge.Resolver]` where structs of the same type that
   implement the protocol are deeply merged.
   """
   def resolve(original = %{__struct__: struct}, override = %{__struct__: struct}, resolver) do
-    implementors = get_implementors(DeepMerge.Resolver.__protocol__(:impls))
-
-    if Enum.member?(implementors, struct) do
-      Map.merge(original, override, resolver)
-    else
+    if DeepMerge.Resolver.impl_for(original) == DeepMerge.Resolver.Any do
       override
+    else
+      Map.merge(original, override, resolver)
     end
   end
 
   def resolve(_original, override, _fun), do: override
-
-  defp get_implementors({:consolidated, implementors}), do: implementors
-
-  defp get_implementors(:not_consolidated) do
-    IO.warn(
-      "Protocols not consolidated and trying to merge two structs of the same type. Not supported!"
-    )
-
-    # let the code work with override semantics without being intrusive
-    []
-  end
 end
